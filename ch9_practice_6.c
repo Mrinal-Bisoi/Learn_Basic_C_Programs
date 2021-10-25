8b9b2f882eafb436e867cbaad1051c161b576231
Mrinal <t20cs003@nitm.ac.in>
2021-10-25 09:12:42 +0530

    Confirm

---
+++ ch9_practice_6.c
@@ -0,0 +1,26 @@
+// Create an array of 5 complex numbers and display them with the help of a display function.The values must be taken as user input.
+
+#include<stdio.h>
+
+typedef struct complex
+{
+    int real;
+    int img;
+} cmp;
+
+cmp display(cmp complex, int i){
+    printf("Complex number_%d :  %d + i%d \n", i+1, complex.real, complex.img);
+    i++;
+}
+
+int main(){
+    cmp complex[5];
+    for(int i = 0; i < 5; i++){
+        printf("Enter the real part and imaginary part of complex number%d : ", i+1);
+        scanf("%d  %d", &complex[i].real, &complex[i].img);
+    }
+    for(int i = 0; i < 5; i++){
+        display(complex[i], i);
+    }
+    return 0;
+}
